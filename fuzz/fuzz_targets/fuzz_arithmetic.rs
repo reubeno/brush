aa7dc9f18b53af660a975a1c61fcc14001926848
reuben olinsky <reubeno@users.noreply.github.com>
2024-07-01 14:15:18 -0700

    feat: enable fuzz-testing arithmetic eval

---
+++ fuzz/fuzz_targets/fuzz_arithmetic.rs
@@ -0,0 +1,78 @@
+#![no_main]
+
+use anyhow::Result;
+use brush_parser::ast;
+use libfuzzer_sys::fuzz_target;
+
+async fn eval_arithmetic_async(input: ast::ArithmeticExpr) -> Result<()> {
+    //
+    // Turn it back into a string so we can pass it in on the command-line.
+    //
+    let input_str = input.to_string();
+
+    //
+    // Instantiate a brush shell with defaults, then try to evaluate the expression.
+    //
+    let options = brush_core::CreateOptions::default();
+    let mut shell = brush_core::Shell::new(&options).await?;
+    let parsed_expr = brush_parser::arithmetic::parse(input_str.as_str()).ok();
+    let our_eval_result = if let Some(parsed_expr) = parsed_expr {
+        shell.eval_arithmetic(parsed_expr).await.ok()
+    } else {
+        None
+    };
+
+    //
+    // Now run it under 'bash'
+    //
+    let mut oracle_cmd = std::process::Command::new("bash");
+    oracle_cmd
+        .arg("--noprofile")
+        .arg("--norc")
+        .arg("-O")
+        .arg("extglob")
+        .arg("-t");
+
+    let mut oracle_cmd = assert_cmd::Command::from_std(oracle_cmd);
+
+    const DEFAULT_TIMEOUT_IN_SECONDS: u64 = 15;
+    oracle_cmd.timeout(std::time::Duration::from_secs(DEFAULT_TIMEOUT_IN_SECONDS));
+
+    let input = std::format!("echo \"$(( {input} ))\"\n");
+    oracle_cmd.write_stdin(input.as_bytes());
+
+    let oracle_result = oracle_cmd.output()?;
+    let oracle_eval_result = if oracle_result.status.success() {
+        let oracle_output = std::str::from_utf8(&oracle_result.stdout)?;
+        oracle_output.trim().parse::<i64>().ok()
+    } else {
+        None
+    };
+
+    //
+    // Compare results.
+    //
+    if our_eval_result != oracle_eval_result {
+        Err(anyhow::anyhow!(
+            "Mismatched eval results: {oracle_eval_result:?} from oracle vs. {our_eval_result:?} from our test (expr: '{input}', oracle result: {oracle_result:?})"
+        ))
+    } else {
+        Ok(())
+    }
+}
+
+fuzz_target!(|input: ast::ArithmeticExpr| {
+    let s = input.to_string();
+    let s = s.trim();
+
+    // For now, intentionally ignore known problematic cases without actually running them.
+    if s.contains("+ 0")
+        || s.is_empty()
+        || s.contains(|c: char| c.is_ascii_control() || !c.is_ascii())
+    {
+        return;
+    }
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(eval_arithmetic_async(input)).unwrap();
+});
