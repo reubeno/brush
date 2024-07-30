d917de8e58c2a1a6b5eab38a826370148a10aabe
reuben olinsky <reubeno@users.noreply.github.com>
2024-07-30 09:45:30 -0700

    fix: correct echo -e escape expansion for \x sequences (#143)

--- brush-core/src/escape.rs
+++ brush-core/src/escape.rs
@@ -12,155 +12,201 @@ pub(crate) enum EscapeMode {
 pub(crate) fn expand_backslash_escapes(
     s: &str,
     mode: EscapeMode,
-) -> Result<(String, bool), crate::error::Error> {
-    let mut result = String::new();
+) -> Result<(Vec<u8>, bool), crate::error::Error> {
+    let mut result: Vec<u8> = vec![];
     let mut it = s.chars();
     while let Some(c) = it.next() {
-        if c == '\\' {
-            if let Some(next) = it.next() {
-                match next {
-                    'a' => result.push('\x07'),
-                    'b' => result.push('\x08'),
-                    'c' => {
-                        match mode {
-                            EscapeMode::EchoBuiltin => {
-                                // Stop all additional output!
-                                return Ok((result, false));
-                            }
-                            EscapeMode::AnsiCQuotes => {
-                                if let Some(_next_next) = it.next() {
-                                    return error::unimp("control character in ANSI C quotes");
-                                } else {
-                                    result.push('\\');
-                                    result.push('c');
-                                }
-                            }
-                        }
-                    }
-                    'e' | 'E' => result.push('\x1b'),
-                    'f' => result.push('\x0c'),
-                    'n' => result.push('\n'),
-                    'r' => result.push('\r'),
-                    't' => result.push('\t'),
-                    'v' => result.push('\x0b'),
-                    '\\' => result.push('\\'),
-                    '\'' if matches!(mode, EscapeMode::AnsiCQuotes) => result.push('\''),
-                    '\"' if matches!(mode, EscapeMode::AnsiCQuotes) => result.push('\"'),
-                    '?' if matches!(mode, EscapeMode::AnsiCQuotes) => result.push('?'),
-                    '0' => {
-                        // Consume 0-3 valid octal chars
-                        let mut taken_so_far = 0;
-                        let mut octal_chars: String = it
-                            .take_while_ref(|c| {
-                                if taken_so_far < 3 && matches!(*c, '0'..='7') {
-                                    taken_so_far += 1;
-                                    true
-                                } else {
-                                    false
-                                }
-                            })
-                            .collect();
-
-                        if octal_chars.is_empty() {
-                            octal_chars.push('0');
-                        }
-
-                        let value = u8::from_str_radix(octal_chars.as_str(), 8)?;
-
-                        // TODO: Should really parse as ASCII.
-                        result.push_str(std::str::from_utf8(&[value])?);
-                    }
-                    'x' => {
-                        // Consume 1-2 valid hex chars
-                        let mut taken_so_far = 0;
-                        let hex_chars: String = it
-                            .take_while_ref(|c| {
-                                if taken_so_far < 2 && c.is_ascii_hexdigit() {
-                                    taken_so_far += 1;
-                                    true
-                                } else {
-                                    false
-                                }
-                            })
-                            .collect();
-
-                        if hex_chars.is_empty() {
-                            result.push('\\');
-                            result.push(c);
-                        } else {
-                            let value = u8::from_str_radix(hex_chars.as_str(), 16)?;
-
-                            // TODO: Should really parse as ASCII.
-                            result.push_str(std::str::from_utf8(&[value])?);
-                        }
-                    }
-                    'u' => {
-                        // Consume 1-4 hex digits
-                        let mut taken_so_far = 0;
-                        let hex_chars: String = it
-                            .take_while_ref(|c| {
-                                if taken_so_far < 4 && c.is_ascii_hexdigit() {
-                                    taken_so_far += 1;
-                                    true
-                                } else {
-                                    false
-                                }
-                            })
-                            .collect();
-
-                        if hex_chars.is_empty() {
-                            result.push('\\');
-                            result.push(c);
-                        } else {
-                            let value = u16::from_str_radix(hex_chars.as_str(), 16)?;
-
-                            if let Some(decoded) = char::from_u32(u32::from(value)) {
-                                result.push(decoded);
-                            } else {
-                                result.push('\\');
-                                result.push(c);
-                            }
-                        }
-                    }
-                    'U' => {
-                        // Consume 1-8 hex digits
-                        let mut taken_so_far = 0;
-                        let hex_chars: String = it
-                            .take_while_ref(|c| {
-                                if taken_so_far < 8 && c.is_ascii_hexdigit() {
-                                    taken_so_far += 1;
-                                    true
-                                } else {
-                                    false
-                                }
-                            })
-                            .collect();
-
-                        if hex_chars.is_empty() {
-                            result.push('\\');
-                            result.push(c);
-                        } else {
-                            let value = u32::from_str_radix(hex_chars.as_str(), 16)?;
-
-                            if let Some(decoded) = char::from_u32(value) {
-                                result.push(decoded);
-                            } else {
-                                result.push('\\');
-                                result.push(c);
-                            }
-                        }
-                    }
-                    _ => result.push(c),
-                }
-            } else {
-                // It's a trailing backslash, add it.
-                result.push(c);
-            }
-        } else {
+        if c != '\\' {
             // Not a backslash, add and move on.
-            result.push(c);
+            result.append(c.to_string().into_bytes().as_mut());
+            continue;
+        }
+
+        match it.next() {
+            Some('a') => result.push(b'\x07'),
+            Some('b') => result.push(b'\x08'),
+            Some('c') => {
+                match mode {
+                    EscapeMode::EchoBuiltin => {
+                        // Stop all additional output!
+                        return Ok((result, false));
+                    }
+                    EscapeMode::AnsiCQuotes => {
+                        if let Some(_next_next) = it.next() {
+                            return error::unimp("control character in ANSI C quotes");
+                        } else {
+                            result.push(b'\\');
+                            result.push(b'c');
+                        }
+                    }
+                }
+            }
+            Some('e' | 'E') => result.push(b'\x1b'),
+            Some('f') => result.push(b'\x0c'),
+            Some('n') => result.push(b'\n'),
+            Some('r') => result.push(b'\r'),
+            Some('t') => result.push(b'\t'),
+            Some('v') => result.push(b'\x0b'),
+            Some('\\') => result.push(b'\\'),
+            Some('\'') if matches!(mode, EscapeMode::AnsiCQuotes) => result.push(b'\''),
+            Some('\"') if matches!(mode, EscapeMode::AnsiCQuotes) => result.push(b'\"'),
+            Some('?') if matches!(mode, EscapeMode::AnsiCQuotes) => result.push(b'?'),
+            Some('0') => {
+                // Consume 0-3 valid octal chars
+                let mut taken_so_far = 0;
+                let mut octal_chars: String = it
+                    .take_while_ref(|c| {
+                        if taken_so_far < 3 && matches!(*c, '0'..='7') {
+                            taken_so_far += 1;
+                            true
+                        } else {
+                            false
+                        }
+                    })
+                    .collect();
+
+                if octal_chars.is_empty() {
+                    octal_chars.push('0');
+                }
+
+                let value = u8::from_str_radix(octal_chars.as_str(), 8)?;
+                result.push(value);
+            }
+            Some('x') => {
+                // Consume 1-2 valid hex chars
+                let mut taken_so_far = 0;
+                let hex_chars: String = it
+                    .take_while_ref(|c| {
+                        if taken_so_far < 2 && c.is_ascii_hexdigit() {
+                            taken_so_far += 1;
+                            true
+                        } else {
+                            false
+                        }
+                    })
+                    .collect();
+
+                if hex_chars.is_empty() {
+                    result.push(b'\\');
+                    result.append(c.to_string().into_bytes().as_mut());
+                } else {
+                    let value = u8::from_str_radix(hex_chars.as_str(), 16)?;
+                    result.push(value);
+                }
+            }
+            Some('u') => {
+                // Consume 1-4 hex digits
+                let mut taken_so_far = 0;
+                let hex_chars: String = it
+                    .take_while_ref(|c| {
+                        if taken_so_far < 4 && c.is_ascii_hexdigit() {
+                            taken_so_far += 1;
+                            true
+                        } else {
+                            false
+                        }
+                    })
+                    .collect();
+
+                if hex_chars.is_empty() {
+                    result.push(b'\\');
+                    result.append(c.to_string().into_bytes().as_mut());
+                } else {
+                    let value = u16::from_str_radix(hex_chars.as_str(), 16)?;
+
+                    if let Some(decoded) = char::from_u32(u32::from(value)) {
+                        result.append(decoded.to_string().into_bytes().as_mut());
+                    } else {
+                        result.push(b'\\');
+                        result.append(c.to_string().into_bytes().as_mut());
+                    }
+                }
+            }
+            Some('U') => {
+                // Consume 1-8 hex digits
+                let mut taken_so_far = 0;
+                let hex_chars: String = it
+                    .take_while_ref(|c| {
+                        if taken_so_far < 8 && c.is_ascii_hexdigit() {
+                            taken_so_far += 1;
+                            true
+                        } else {
+                            false
+                        }
+                    })
+                    .collect();
+
+                if hex_chars.is_empty() {
+                    result.push(b'\\');
+                    result.append(c.to_string().into_bytes().as_mut());
+                } else {
+                    let value = u32::from_str_radix(hex_chars.as_str(), 16)?;
+
+                    if let Some(decoded) = char::from_u32(value) {
+                        result.append(decoded.to_string().into_bytes().as_mut());
+                    } else {
+                        result.push(b'\\');
+                        result.append(c.to_string().into_bytes().as_mut());
+                    }
+                }
+            }
+            Some(c) => {
+                // Not a valid escape sequence.
+                result.push(b'\\');
+                result.append(c.to_string().into_bytes().as_mut());
+            }
+            None => {
+                // Trailing backslash.
+                result.push(b'\\');
+            }
         }
     }
 
     Ok((result, true))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_echo_expands_to(unexpanded: &str, expected: &str) {
+        assert_eq!(
+            String::from_utf8(
+                expand_backslash_escapes(unexpanded, EscapeMode::EchoBuiltin)
+                    .unwrap()
+                    .0
+            )
+            .unwrap(),
+            expected
+        );
+    }
+
+    #[test]
+    fn test_echo_expansion() {
+        assert_echo_expands_to("a", "a");
+        assert_echo_expands_to(r"\M", "\\M");
+        assert_echo_expands_to(r"a\nb", "a\nb");
+        assert_echo_expands_to(r"\a", "\x07");
+        assert_echo_expands_to(r"\b", "\x08");
+        assert_echo_expands_to(r"\e", "\x1b");
+        assert_echo_expands_to(r"\f", "\x0c");
+        assert_echo_expands_to(r"\n", "\n");
+        assert_echo_expands_to(r"\r", "\r");
+        assert_echo_expands_to(r"\t", "\t");
+        assert_echo_expands_to(r"\v", "\x0b");
+        assert_echo_expands_to(r"\\", "\\");
+        assert_echo_expands_to(r"\'", "\\'");
+        assert_echo_expands_to(r#"\""#, r#"\""#);
+        assert_echo_expands_to(r"\?", "\\?");
+        assert_echo_expands_to(r"\0", "\0");
+        assert_echo_expands_to(r"\00", "\0");
+        assert_echo_expands_to(r"\000", "\0");
+        assert_echo_expands_to(r"\081", "\081");
+        assert_echo_expands_to(r"\0101", "A");
+        assert_echo_expands_to(r"abc\", "abc\\");
+        assert_echo_expands_to(r"\x41", "A");
+        assert_echo_expands_to(r"\xf0\x9f\x90\x8d", "🐍");
+        assert_echo_expands_to(r"\u2620", "☠");
+        assert_echo_expands_to(r"\U0001f602", "😂");
+    }
 }
