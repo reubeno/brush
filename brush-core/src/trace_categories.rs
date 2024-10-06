f434e79cee4384782910f17d67fdc89236c7a00b
reuben olinsky <reubeno@users.noreply.github.com>
2024-10-06 15:13:40 -0700

    fix: deduplicate completion candidates (#189)

--- brush-core/src/trace_categories.rs
+++ brush-core/src/trace_categories.rs
@@ -1,3 +1,4 @@
 pub(crate) const COMMANDS: &str = "commands";
+pub(crate) const COMPLETION: &str = "completion";
 pub(crate) const JOBS: &str = "jobs";
 pub(crate) const PARSE: &str = "parse";
--- brush-interactive/src/completion.rs
+++ brush-interactive/src/completion.rs
@@ -1,5 +1,7 @@
 use std::path::{Path, PathBuf};
 
+use indexmap::IndexSet;
+
 use crate::trace_categories;
 
 pub(crate) async fn complete_async(
@@ -27,40 +29,57 @@ pub(crate) async fn complete_async(
     let mut completions = result.unwrap_or_else(|_| brush_core::completion::Completions {
         insertion_index: pos,
         delete_count: 0,
-        candidates: vec![],
+        candidates: IndexSet::new(),
         options: brush_core::completion::ProcessingOptions::default(),
     });
 
+    // TODO: Consider optimizing this out when not needed?
     let completing_end_of_line = pos == line.len();
-    if completions.options.treat_as_filenames {
-        for candidate in &mut completions.candidates {
-            // Check if it's a directory.
-            if !candidate.ends_with(std::path::MAIN_SEPARATOR) {
-                let candidate_path = Path::new(candidate);
-                let abs_candidate_path = if candidate_path.is_absolute() {
-                    PathBuf::from(candidate_path)
-                } else {
-                    working_dir.join(candidate_path)
-                };
-
-                if abs_candidate_path.is_dir() {
-                    candidate.push(std::path::MAIN_SEPARATOR);
-                }
-            }
-        }
-    }
-    if completions.options.no_autoquote_filenames {
-        tracing::debug!(target: trace_categories::COMPLETION, "don't autoquote filenames");
-    }
-    if completing_end_of_line && !completions.options.no_trailing_space_at_end_of_line {
-        for candidate in &mut completions.candidates {
-            if !completions.options.treat_as_filenames
-                || !candidate.ends_with(std::path::MAIN_SEPARATOR)
-            {
-                candidate.push(' ');
-            }
-        }
-    }
+    completions.candidates = completions
+        .candidates
+        .into_iter()
+        .map(|candidate| {
+            postprocess_completion_candidate(
+                candidate,
+                &completions.options,
+                working_dir.as_ref(),
+                completing_end_of_line,
+            )
+        })
+        .collect();
 
     completions
+}
+
+fn postprocess_completion_candidate(
+    mut candidate: String,
+    options: &brush_core::completion::ProcessingOptions,
+    working_dir: &Path,
+    completing_end_of_line: bool,
+) -> String {
+    if options.treat_as_filenames {
+        // Check if it's a directory.
+        if !candidate.ends_with(std::path::MAIN_SEPARATOR) {
+            let candidate_path = Path::new(&candidate);
+            let abs_candidate_path = if candidate_path.is_absolute() {
+                PathBuf::from(candidate_path)
+            } else {
+                working_dir.join(candidate_path)
+            };
+
+            if abs_candidate_path.is_dir() {
+                candidate.push(std::path::MAIN_SEPARATOR);
+            }
+        }
+    }
+    if options.no_autoquote_filenames {
+        tracing::debug!(target: trace_categories::COMPLETION, "don't autoquote filenames");
+    }
+    if completing_end_of_line && !options.no_trailing_space_at_end_of_line {
+        if !options.treat_as_filenames || !candidate.ends_with(std::path::MAIN_SEPARATOR) {
+            candidate.push(' ');
+        }
+    }
+
+    candidate
 }
