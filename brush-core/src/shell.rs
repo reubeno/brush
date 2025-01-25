c1eaf01c7f11f0765c8c5c3ab3400fb58aa15a19
reuben olinsky <reubeno@users.noreply.github.com>
2025-01-25 09:50:34 -0800

    perf: revert change to expand PS* in subshell

--- brush-core/src/shell.rs
+++ brush-core/src/shell.rs
@@ -1075,46 +1075,47 @@ impl Shell {
 
     /// Composes the shell's post-input, pre-command prompt, applying all appropriate expansions.
     pub async fn compose_precmd_prompt(&mut self) -> Result<String, error::Error> {
-        self.expand_prompt_var_in_subshell("PS0", "").await
+        self.expand_prompt_var("PS0", "").await
     }
 
     /// Composes the shell's prompt, applying all appropriate expansions.
     pub async fn compose_prompt(&mut self) -> Result<String, error::Error> {
-        self.expand_prompt_var_in_subshell("PS1", self.default_prompt())
-            .await
+        self.expand_prompt_var("PS1", self.default_prompt()).await
     }
 
     /// Compose's the shell's alternate-side prompt, applying all appropriate expansions.
     #[allow(clippy::unused_async)]
     pub async fn compose_alt_side_prompt(&mut self) -> Result<String, error::Error> {
         // This is a brush extension.
-        self.expand_prompt_var_in_subshell("BRUSH_PS_ALT", "").await
+        self.expand_prompt_var("BRUSH_PS_ALT", "").await
     }
 
     /// Composes the shell's continuation prompt.
     pub async fn compose_continuation_prompt(&mut self) -> Result<String, error::Error> {
-        self.expand_prompt_var_in_subshell("PS2", "> ").await
+        self.expand_prompt_var("PS2", "> ").await
     }
 
-    async fn expand_prompt_var_in_subshell(
+    async fn expand_prompt_var(
         &mut self,
         var_name: &str,
         default: &str,
     ) -> Result<String, error::Error> {
-        // Create subshell.
-        let mut subshell = self.clone();
+        //
+        // TODO(prompt): bash appears to do this in a subshell; we need to investigate
+        // if that's required.
+        //
 
         // Retrieve the spec.
-        let prompt_spec = subshell.parameter_or_default(var_name, default);
+        let prompt_spec = self.parameter_or_default(var_name, default);
         if prompt_spec.is_empty() {
             return Ok(String::new());
         }
 
         // Expand it.
-        let formatted_prompt = prompt::expand_prompt(&subshell, prompt_spec.into_owned())?;
+        let formatted_prompt = prompt::expand_prompt(self, prompt_spec.into_owned())?;
 
         // Now expand.
-        expansion::basic_expand_str(&mut subshell, &formatted_prompt).await
+        expansion::basic_expand_str(self, &formatted_prompt).await
     }
 
     /// Returns the exit status of the last command executed in this shell.
