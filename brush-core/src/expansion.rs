734c60f7df9a055009bbf6edf9b386a72f4c2e4e
reuben olinsky <reubeno@users.noreply.github.com>
2025-05-12 13:51:46 -0700

    fix: remove tracing

--- brush-core/src/expansion.rs
+++ brush-core/src/expansion.rs
@@ -391,14 +391,7 @@ impl<'a> WordExpander<'a> {
 
     /// Apply tilde-expansion, parameter expansion, command substitution, and arithmetic expansion.
     pub async fn basic_expand_to_str(&mut self, word: &str) -> Result<String, error::Error> {
-        let expanded = String::from(self.basic_expand(word).await?);
-
-        tracing::debug!(
-            target: trace_categories::EXPANSION,
-            "Basic expanded: '{word}' => '{expanded}'"
-        );
-
-        Ok(expanded)
+        String::from(self.basic_expand(word).await?)
     }
 
     #[allow(clippy::ref_option)]
