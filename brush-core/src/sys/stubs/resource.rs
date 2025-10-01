0ae5a2f88264261e3b14421e11337b631a646c7c
reuben olinsky <reubeno@users.noreply.github.com>
2025-10-01 13:27:38 -0700

    chore: fix clippy warning

--- brush-core/src/sys/stubs/resource.rs
+++ brush-core/src/sys/stubs/resource.rs
@@ -3,16 +3,16 @@
 use crate::error;
 
 /// Returns the user and system CPU time used by the current process.
+///
 /// This is a stub implementation that returns zero durations.
-#[expect(clippy::unnecessary_wraps)]
 pub fn get_self_user_and_system_time()
 -> Result<(std::time::Duration, std::time::Duration), error::Error> {
     Ok((std::time::Duration::ZERO, std::time::Duration::ZERO))
 }
 
 /// Returns the user and system CPU time used by child processes.
+///
 /// This is a stub implementation that returns zero durations.
-#[expect(clippy::unnecessary_wraps)]
 pub fn get_children_user_and_system_time()
 -> Result<(std::time::Duration, std::time::Duration), error::Error> {
     Ok((std::time::Duration::ZERO, std::time::Duration::ZERO))
