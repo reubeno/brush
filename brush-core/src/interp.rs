403dee1efff5533dbb4ccd8bee9caae103e17d21
Luca Barbato <lu_zero@gentoo.org>
2025-10-12 06:01:39 +0200

    feat: Update brush-core

--- brush-core/src/interp.rs
+++ brush-core/src/interp.rs
@@ -487,7 +487,9 @@ impl Execute for ast::CompoundCommand {
         params: &ExecutionParameters,
     ) -> Result<ExecutionResult, error::Error> {
         match self {
-            Self::BraceGroup(ast::BraceGroupCommand(g)) => g.execute(shell, params).await,
+            Self::BraceGroup(ast::BraceGroupCommand { list, .. }) => {
+                list.execute(shell, params).await
+            }
             Self::Subshell(ast::SubshellCommand(s)) => {
                 // Clone off a new subshell, and run the body of the subshell there.
                 let mut subshell = shell.clone();
@@ -557,7 +559,7 @@ impl Execute for ast::ForClauseCommand {
                 EnvironmentScope::Global,
             )?;
 
-            result = self.body.0.execute(shell, params).await?;
+            result = self.body.list.execute(shell, params).await?;
             if result.is_return_or_exit() {
                 break;
             }
@@ -721,7 +723,7 @@ impl Execute for (WhileOrUntil, &ast::WhileOrUntilClauseCommand) {
                 break;
             }
 
-            result = body.0.execute(shell, params).await?;
+            result = body.list.execute(shell, params).await?;
             if result.is_return_or_exit() {
                 break;
             }
@@ -778,7 +780,7 @@ impl Execute for ast::ArithmeticForClauseCommand {
                 }
             }
 
-            result = self.body.0.execute(shell, params).await?;
+            result = self.body.list.execute(shell, params).await?;
             if result.is_return_or_exit() {
                 break;
             }
@@ -808,7 +810,7 @@ impl Execute for ast::FunctionDefinition {
         shell: &mut Shell,
         _params: &ExecutionParameters,
     ) -> Result<ExecutionResult, error::Error> {
-        shell.define_func(self.fname.clone(), self.clone());
+        shell.define_func(self.fname.value.clone(), self.clone());
 
         let result = ExecutionResult::success();
         *shell.last_exit_status_mut() = result.exit_code.into();
