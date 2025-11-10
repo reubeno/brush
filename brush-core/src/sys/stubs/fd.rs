3f979c1ccf72db8633928d94e46db5ebf5f5f6fe
reuben olinsky <reubeno@users.noreply.github.com>
2025-11-10 07:46:01 -0800

    fix: plat issue

--- brush-core/src/sys/stubs/fd.rs
+++ brush-core/src/sys/stubs/fd.rs
@@ -3,11 +3,11 @@
 use crate::{error, openfiles};
 
 /// Stub implementation for platforms that do not support enumerating file descriptors.
-pub fn try_iter_open_fds() -> impl Iterator<Item = (ShellFd, openfiles::OpenFile)> {
+pub fn try_iter_open_fds() -> impl Iterator<Item = (i32, openfiles::OpenFile)> {
     std::iter::empty()
 }
 
 /// Stub implementation for platforms that do not support opening file descriptors.
-pub fn try_get_file_for_open_fd(_fd: RawFd) -> Option<openfiles::OpenFile> {
+pub fn try_get_file_for_open_fd(_fd: i32) -> Option<openfiles::OpenFile> {
     None
 }
