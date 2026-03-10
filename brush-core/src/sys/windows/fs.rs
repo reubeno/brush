c59e8b81d7bdd2ccb2abf3f4d43c99cd7d5de74e
Brian McKenna <brian@brianmckenna.org>
2026-03-10 17:27:43 +1100

    feat: add /dev/null handling
    This allows some scripts to run on Windows, which otherwise fail
    because the /dev/null device does not exist. This is similar to the
    existing special /dev/fd handling which Brush already contains.

--- brush-core/src/sys/windows/fs.rs
+++ brush-core/src/sys/windows/fs.rs
@@ -2,9 +2,20 @@
 
 pub use crate::sys::stubs::fs::*;
 
+use crate::error;
+
 /// Splits a platform-specific PATH-like value into individual paths.
 ///
 /// On Windows, this delegates to [`std::env::split_paths`].
 pub fn split_paths<T: AsRef<std::ffi::OsStr> + ?Sized>(s: &T) -> std::env::SplitPaths<'_> {
     std::env::split_paths(s)
+}
+
+/// Opens a null file that will discard all I/O.
+pub fn open_null_file() -> Result<std::fs::File, error::Error> {
+    let f = std::fs::File::options()
+        .read(true)
+        .write(true)
+        .open("NUL")?;
+    Ok(f)
 }
